6700fd09e69c830e4e2b2f5ba17417334d514d03
NataliaColome <nmcolome@gmail.com>
2017-06-06 10:08:58 -0600

    write tag model test to assess it has a name and that it has many jobs associated

---
+++ spec/models/tag_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe Tag do
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without a name" do
+        tag = Tag.new
+        expect(tag).to be_invalid
+      end
+
+    context "valid attributes" do
+      it "is valid with a name" do
+        tag = Tag.new(name: 'Software')
+        expect(tag).to be_valid
+      end
+    end
+  end
+
+  describe "relationships" do
+    it "has many jobs" do
+      tag = Tag.new(name: 'Software')
+      expect(tag).to respond_to(:jobs)
+    end
+  end
+end
