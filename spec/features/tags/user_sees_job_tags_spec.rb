84011a8b00322bf057c398e40d492b667cef6b05
NataliaColome <nmcolome@gmail.com>
2017-06-06 10:05:42 -0600

    set up and write user stories for tags

---
+++ spec/features/tags/user_sees_job_tags_spec.rb
@@ -0,0 +1,3 @@
+# As a user,
+# When I see a specific job page,
+# I see the name of each of the tags associated with that job.
---
+++ spec/features/tags/user_sees_jobs_associated_to_a_tag_spec.rb
@@ -0,0 +1,3 @@
+# As a user,
+# When I visit a specific job page,
+# I also see a count of how many jobs have each specific tag listed.
